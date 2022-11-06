58b4cfb1347d962a2693db45ae99be994de06e3b
Peter Kang <pxk7062@rit.edu>
2022-11-06 16:11:40 -0500

    Sensors on table done

---
+++ TableSensors/TableSensors.ino
@@ -0,0 +1,45 @@
+#include <SoftwareSerial.h>
+
+//Analog sensorPorts
+int sensorPort0 = 0;
+int sensorPort1 = 1;
+int sensorPort2 = 2;
+int sensorPort3 = 3;
+int sensorPort4 = 4;
+int sensorPort5 = 5;
+//Analog sensorPort Values
+int sensorPortValue0 = 0;
+int sensorPortValue1 = 0;
+int sensorPortValue2 = 0;
+int sensorPortValue3 = 0;
+int sensorPortValue4 = 0;
+int sensorPortValue5 = 0;
+//Max Value
+int maxTotalValue = 1000;
+
+//Loop Delay timer
+int waitDelayTimeInMS = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // baud rate
+}
+
+void loop() {
+  //Read in the values
+  sensorPortValue0 = analogRead(sensorPort0);
+  sensorPortValue1 = analogRead(sensorPort1);
+  sensorPortValue2 = analogRead(sensorPort2);
+  sensorPortValue3 = analogRead(sensorPort3);
+  sensorPortValue4 = analogRead(sensorPort4);
+  sensorPortValue5 = analogRead(sensorPort5);
+  // Print findings
+  Serial.println((String)"Values: " +sensorPort0+", "+sensorPort1+", "+ sensorPort2+", "+sensorPort3+", "+sensorPort4+", "+sensorPort5+".\n"); 
+  //Add up the values and see if they are above the threshhold
+  int totalValue = sensorPortValue0 + sensorPortValue1 + sensorPortValue2 + sensorPortValue3 + sensorPortValue4 + sensorPortValue5;
+  Serial.println((String) "Total: " + totalValue);
+
+
+
+  delay(waitDelayTimerInMS);//delay in miliseconds
+}
