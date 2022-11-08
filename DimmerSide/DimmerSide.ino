e27a91a3111191c39336c87cea4429cf08f760e3
Peter Kang <pxk7062@rit.edu>
2022-11-08 16:07:26 -0500

    Updated values to take into account dimmer values

--- DimmerSide/DimmerSide.ino
+++ DimmerSide/DimmerSide.ino
@@ -2,6 +2,8 @@ int led = 9;         // the PWM pin the LED is attached to
 int inverseled = 10;
 int brightness = 0;  // how bright the LED is
 int maxBrightness = 255;
+int minPWM = 25;
+int maxPWN = 95;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -21,8 +23,15 @@ void loop() {
     Message = Serial.read(); // Reads the data from the serial port
     inverseMessage = 255-Message;
     Serial.println(Message);
-    analogWrite(led, Message);
-    analogWrite(inverseled, inverseMessage);
+    
+    //Convert Message value to Dimmer Value
+    double ratio = Message/255.0;
+    double inverseRatio = 1-ratio;
+    int DimmerValue = (ratio*95);
+    int inverseDimmerValue = (inverseRatio*95);
+
+    analogWrite(led, DimmerValue);
+    analogWrite(inverseled, inverseDimmerValue);
   }
   
   // wait for 30 milliseconds to see the dimming effect
--- TableSensors/TableSensors.ino
+++ TableSensors/TableSensors.ino
@@ -28,13 +28,19 @@ void loop() {
   sensorPortValue4 = analogRead(sensorPort4);
   sensorPortValue5 = analogRead(sensorPort5);
   // Print findings
-  //Serial.println((String)"Values: " +sensorPortValue0+", "+sensorPortValue1+", "+ sensorPortValue2+", "+sensorPortValue3+", "+sensorPortValue4+", "+sensorPortValue5+"."); 
+  // Serial.println((String)"Values: " +sensorPortValue0+", "+sensorPortValue1+", "+ sensorPortValue2+", "+sensorPortValue3+", "+sensorPortValue4+", "+sensorPortValue5+"."); 
   //Add up the values and see if they are above the threshhold
   int totalValue = sensorPortValue0 + sensorPortValue1 + sensorPortValue2 + sensorPortValue3 + sensorPortValue4 + sensorPortValue5;
+  //Serial.println((String)"Total Value: "+totalValue);
   double ratio = totalValue/2500.0;
   if(ratio > 1.0)
   {
     ratio =1.0;
+  }
+
+  if(ratio < 0.0)
+  {
+    ratio = 0.0;
   }
   int brightness = ratio*255;
   //Serial.println((String) "Brightness: " + brightness);
