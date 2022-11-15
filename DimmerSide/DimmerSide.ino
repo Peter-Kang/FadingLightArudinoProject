06b38247b6bc601a6057a5e8056d860e1dcedf3c
Peter Kang <pxk7062@rit.edu>
2022-11-14 22:41:12 -0500

    Sensor side with more hardware testing

--- DimmerSide/DimmerSide.ino
+++ DimmerSide/DimmerSide.ino
@@ -1,39 +1,55 @@
-int led = 9;         // the PWM pin the LED is attached to
-int inverseled = 10;
-int brightness = 0;  // how bright the LED is
-int maxBrightness = 255;
-int minPWM = 25;
-int maxPWN = 95;
+//#include <RBDdimmer.h>
+//LED locations
+int led = 10;         // the PWM pin the LED is attached to
+int inverseled = 9;
+//Constants
+int maxValue = 6;
+  //Dimmer values
+int minPWM = 0;
+int maxPWN = 100;
+
+int Message = 0;
+int inverseMessage =0;
+
+//dimmerLamp dimmer(led);
+//dimmerLamp inverseDimmer(inverseled);
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
   pinMode(led, OUTPUT);
   pinMode(inverseled, OUTPUT);
+  //inverseDimmer.begin(NORMAL_MODE, ON);
+  //dimmer.begin(NORMAL_MODE, ON);
   Serial.begin(38400); // baud rate
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  int Message = 0;
-  int inverseMessage =0;
+ 
   //Take a message from the bluetooth device
   Serial.flush();
   if(Serial.available() > 0){ // Checks whether data is comming from the serial port
     Message = Serial.read(); // Reads the data from the serial port
-    inverseMessage = 255-Message;
+    inverseMessage = maxValue-Message;
     Serial.println(Message);
-    
-    //Convert Message value to Dimmer Value
-    double ratio = Message/255.0;
-    double inverseRatio = 1-ratio;
-    int DimmerValue = (ratio*95);
-    int inverseDimmerValue = (inverseRatio*95);
-
-    analogWrite(led, DimmerValue);
-    analogWrite(inverseled, inverseDimmerValue);
   }
-  
+  //Convert Message value to Dimmer Value
+      //Ratio
+  double ratio = (double)Message/(double)maxValue;
+  double inverseRatio = 1-ratio;
+    //Dimmer value results
+  int DimmerValue = (ratio*maxPWN);
+  int inverseDimmerValue = (inverseRatio*maxPWN);
+  Serial.println( (String) "Dimmer: " + DimmerValue+  " InverseDimmer: "+ inverseDimmerValue );
+
+  //inverseDimmer.setPower(inverseDimmerValue);
+  //dimmer.setPower(DimmerValue);
+  analogWrite(led, DimmerValue);
+  analogWrite(inverseled, inverseDimmerValue);
+//digitalWrite(led, DimmerValue );
+//digitalWrite( inverseled, inverseDimmerValue);
+
   // wait for 30 milliseconds to see the dimming effect
   delay(500);
 }
--- TableSensors/TableSensors.ino
+++ TableSensors/TableSensors.ino
@@ -5,14 +5,27 @@ int sensorPort2 = A2;
 int sensorPort3 = A3;
 int sensorPort4 = A4;
 int sensorPort5 = A5;
-//Analog sensorPort Values
+//Moving average values
+int sensorValue0 = 0;
+int sensorValue1 = 0;
+int sensorValue2 = 0;
+int sensorValue3 = 0;
+int sensorValue4 = 0;
+int sensorValue5 = 0;
+//Analog sensorPort Values / boolean value storage
 int sensorPortValue0 = 0;
 int sensorPortValue1 = 0;
 int sensorPortValue2 = 0;
 int sensorPortValue3 = 0;
 int sensorPortValue4 = 0;
 int sensorPortValue5 = 0;
-
+// Threshhold values for each port
+int sensorPort0Threshhold = 200;
+int sensorPort1Threshhold = 150;
+int sensorPort2Threshhold = 150;
+int sensorPort3Threshhold = 200;
+int sensorPort4Threshhold = 180;
+int sensorPort5Threshhold = 180;
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,25 +40,27 @@ void loop() {
   sensorPortValue3 = analogRead(sensorPort3);
   sensorPortValue4 = analogRead(sensorPort4);
   sensorPortValue5 = analogRead(sensorPort5);
+  //Average the values
+  sensorValue0 = (sensorPortValue0 + sensorValue0 )/2;
+  sensorValue1 = (sensorPortValue1 + sensorValue1 )/2;
+  sensorValue2 = (sensorPortValue2 + sensorValue2 )/2;
+  sensorValue3 = (sensorPortValue3 + sensorValue3 )/2;
+  sensorValue4 = (sensorPortValue4 + sensorValue4 )/2;
+  sensorValue5 = (sensorPortValue5 + sensorValue5 )/2;
+  // Threshhold the values
+  sensorPortValue0 = (int) sensorValue0 > sensorPort0Threshhold;
+  sensorPortValue1 = (int) sensorValue1 > sensorPort1Threshhold;
+  sensorPortValue2 = (int) sensorValue2 > sensorPort2Threshhold;
+  sensorPortValue3 = (int) sensorValue3 > sensorPort3Threshhold;
+  sensorPortValue4 = (int) sensorValue4 > sensorPort4Threshhold;
+  sensorPortValue5 = (int) sensorValue5 > sensorPort5Threshhold;
+
   // Print findings
-  // Serial.println((String)"Values: " +sensorPortValue0+", "+sensorPortValue1+", "+ sensorPortValue2+", "+sensorPortValue3+", "+sensorPortValue4+", "+sensorPortValue5+"."); 
   //Add up the values and see if they are above the threshhold
   int totalValue = sensorPortValue0 + sensorPortValue1 + sensorPortValue2 + sensorPortValue3 + sensorPortValue4 + sensorPortValue5;
   //Serial.println((String)"Total Value: "+totalValue);
-  double ratio = totalValue/2500.0;
-  if(ratio > 1.0)
-  {
-    ratio =1.0;
-  }
-
-  if(ratio < 0.0)
-  {
-    ratio = 0.0;
-  }
-  int brightness = ratio*255;
-  //Serial.println((String) "Brightness: " + brightness);
   //send the value over bluetooth
-  Serial.write(brightness);
+  Serial.write(totalValue);
   Serial.flush();
 
   //Loop Delay timer
